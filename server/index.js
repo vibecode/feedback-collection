1b2af7f0f8a85958fbc798d9bcbf8a5c744925c8
vibecode <rumashkin@gmail.com>
2018-01-11 20:57:49 +0100

    install passport

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const passport = require('passport')
+const GStrat = require('passport-google-oauth20').Strategy
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send( {hi: 'there'})
-})
+passport.use(new GStrat())
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
