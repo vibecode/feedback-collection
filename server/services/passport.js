00e771c6f42e71ba348f61acd7664e8cdf74f90c
vibecode <rumashkin@gmail.com>
2018-01-17 23:04:02 +0100

    save user to database

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,13 +1,16 @@
 const passport = require('passport')
 const GStrat = require('passport-google-oauth20').Strategy
 const keys = require('../config/keys')
+const mongoose = require('mongoose')
+
+const User = mongoose.model('users')
 
 passport.use(new GStrat({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  console.log('access token', accessToken)
-  console.log('refresh token', refreshToken)
-  console.log('profile:', profile);
+  new User({
+    googleId: profile.id
+  }).save()
 }))
