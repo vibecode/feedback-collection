9247e50f57ff97c9d6631b6859bf216db3dbfe8c
vibecode <rumashkin@gmail.com>
2018-01-18 23:35:59 +0100

    enable cookie

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,15 @@ const mongoose = require('mongoose')
 
 const User = mongoose.model('users')
 
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+      .then(user => done(null, user))
+})
+
 passport.use(new GStrat({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
