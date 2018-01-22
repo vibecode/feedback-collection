e5fd038405a11030055b7050e06a4e5a1c973083
vibecode <rumashkin@gmail.com>
2018-01-22 22:54:42 +0100

    refactor to async await syntax

--- server/services/passport.js
+++ server/services/passport.js
@@ -15,19 +15,18 @@ passport.deserializeUser((id, done) => {
 })
 
 passport.use(new GStrat({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback',
-  proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({ googleId: profile.id })
-      .then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser)
-        } else {
-          new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user))
-        }
-      })
-}))
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id })
+
+      if (existingUser) {
+        return done(null, existingUser)
+      }
+
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user)
+    }))
