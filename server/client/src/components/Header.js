52f9488185d079bee9a8e74443edc1da769b9da8
vibecode <rumashkin@gmail.com>
2018-01-26 18:44:28 +0100

    handle login button

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Header extends Component {
-  render(){
-    return(
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return
+      case false:
+        return <li><a href="/auth/google">Login with google</a></li>
+      default:
+        return <li><a href="http://localhost:5000/api/logout">Logout</a></li>
+    }
+  }
+
+  render() {
+    return (
         <nav>
           <div className="nav-wrapper">
             <a className="left brand-logo">
               Home
             </a>
             <ul className="right">
-              <li>
-                <a>Login with Google</a>
-              </li>
+        {this.renderContent()}
             </ul>
           </div>
         </nav>
@@ -19,4 +29,10 @@ class Header extends Component {
   }
 }
 
-export default Header
+const mapStateToProps = ({ auth }) => {
+  return {
+    auth
+  }
+}
+
+export default connect(mapStateToProps, null)(Header)
