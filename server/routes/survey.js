4666fdadd122ef6900adf1a43a875c7ba59e42e2
vibecode <rumashkin@gmail.com>
2018-04-03 18:48:11 +0200

    add survey template

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
 const Mailer = require('../services/mailer')
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
 const Survey = mongoose.model('surveys')
 
@@ -18,6 +19,6 @@ module.exports = app => {
       dateSent: Date.now()
     })
 
-    const Mailer = new Mailer()
+    const Mailer = new Mailer(survey, surveyTemplate(survey))
   })
 }
