38cb07607ae2ab9030b04e9b601f0bbbfe2a09df
vibecode <rumashkin@gmail.com>
2018-04-17 20:09:28 +0200

    implement email validation

---
+++ server/client/src/utils/validateEmails.js
@@ -0,0 +1,12 @@
+const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+export default (emails) => {
+  const invalidEmails = emails
+      .split(',')
+      .map(email => email.trim())
+      .filter(email => regex.test(email) === false)
+  console.log(invalidEmails);
+  if (invalidEmails.length) {
+    return `These emails are invalid: ${invalidEmails}`
+  }
+}
